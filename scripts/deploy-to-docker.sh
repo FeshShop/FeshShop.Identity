d054001599be90552efd1c5ee541899e67f70632
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-10-10 18:31:10 +0300

    Update deploy-to-docker.sh

--- scripts/deploy-to-docker.sh
+++ scripts/deploy-to-docker.sh
@@ -2,5 +2,5 @@
 if [ "$TRAVIS_BRANCH" == "main" ]; then
   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
   docker build .
-  docker push "$DOCKER_USERNAME"/feshshop.identity:latest
+  docker push $DOCKER_USERNAME/feshshop.identity:latest
 fi
