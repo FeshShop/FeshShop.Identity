fbbe9f3e1d8cd2c15c1256d43c0bfd85cf18069c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-10-11 08:46:40 +0300

    update deploy script

--- scripts/deploy-to-docker.sh
+++ scripts/deploy-to-docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "main" ]; then
+  echo "Pushes a local container image to Docker Hub"
+
   echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin docker.io
-  docker build .
+  docker build -t feshShop.identity:latest .
+  docker tag feshShop.identity:$DOCKER_TAG $DOCKER_USERNAME/feshShop.identity:$DOCKER_TAG
   docker push $DOCKER_USERNAME/feshshop.identity:latest
+else
+    echo "not pushing an image, we are not on master"
 fi
